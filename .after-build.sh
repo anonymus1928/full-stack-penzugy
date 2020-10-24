82a48b1de7b2702367241d11b003178f5ffc9fb4
anonymus1928 <anonymus1928@gmail.com>
2020-10-24 13:40:07 +0200

    heroku...

--- .after-build.sh
+++ .after-build.sh
@@ -2,7 +2,3 @@
 
 echo "### Run migration ###"
 php artisan migrate:fresh --force
-echo "### Generate Passport keys ###"
-php artisan passport:keys
-echo "### Encryption keyst to env variables"
-php artisan vendor:publish --tag=passport-config
