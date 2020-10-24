10981066cf5ae932fa307a608af74204d00d05a5
anonymus1928 <anonymus1928@gmail.com>
2020-10-24 11:41:27 +0200

    fix yaml

--- .after-build.sh
+++ .after-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "### Create database ###"
 touch database/database.sqlite
+echo "### .env.heroku => .env ###"
 cp .env.heroku .env
+echo "### Generate application key ###"
 php artisan key:generate --force
+echo "### Run migration ###"
 php artisan migrate --force
+echo "### Generate Passport client ###"
 php artisan passport:install
