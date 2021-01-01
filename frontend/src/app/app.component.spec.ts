92d43897d01530cb6982ca992bcb83a06e7224a5
anonymus1928 <anonymus1928@gmail.com>
2021-01-01 21:44:12 +0100

    angular init

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fullstack-penzugy'`, () => {
+  it(`should have as title 'penzugy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fullstack-penzugy');
+    expect(app.title).toEqual('penzugy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fullstack-penzugy app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('penzugy app is running!');
   });
 });
